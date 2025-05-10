7e88368c315713860cc41b3c784188be1ea58f0f
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2025-05-10 11:02:38 +0200

    🔧 Suppression des imports inutilisés et du code de gestion de session dans le composant RootLayout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/src/providers/ReactQueryProvider";
-import { headers } from "next/headers";
-import { auth } from "../lib/auth";
-import { redirect } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,12 +23,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
 
-  if (!session) redirect("/");
-  
   return (
     <html lang="en">
       <body
